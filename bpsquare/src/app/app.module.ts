3637b85a32336765cbc5a41d1681d2f701a69682
Gustave Gbossou <gustaveg@mac12254.local>
2022-10-05 11:41:18 -0500

    Added images folder, generate slider component and install bootstrap
    Command to install the latest bootstrap module.
    npm install --save bootstrap
    then
    Go to the angular.json file, and add the path: node_modules/bootstrap/dist/css/bootstrap.min.css"  to the styles array:

--- bpsquare/src/app/app.module.ts
+++ bpsquare/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductComponent } from './product/product.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProductComponent
+    ProductComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
