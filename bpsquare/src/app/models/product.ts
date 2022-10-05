64fa8a0ece880054178e0cd91445e996997f7ab7
Gustave Gbossou <gustaveg@mac12254.local>
2022-10-05 08:37:31 -0500

    Create product model
    This is a blueprint of a product with its properties.

---
+++ bpsquare/src/app/models/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+  id!: number;
+  name!: string;
+  price!: number;
+  qty!: number;
+  imagePath!: string;
+  description!: string;
+  isTaxable!: boolean;
+  isImported!: boolean;
+
+  constructor() {}
+}
